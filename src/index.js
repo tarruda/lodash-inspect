31ce23595a59fbac0f5c5072a238d85f5c72ce4d
Adam Krebs <amk528@cs.nyu.edu>
2013-11-22 12:16:10 -0500

    use an array to store methods and found

--- src/index.js
+++ src/index.js
@@ -7,10 +7,6 @@ var escope = require('escope');
 // thanks @akre54(https://github.com/tarruda/lodash-inspect/issues/1) for this trick
 var methods = _.functions(_);
 
-_.each(methods, function(name) {
-  methods[name] = true;
-});
-
 
 function isImplicit(node, scopes) {
   return _.any(scopes, function(scope) {
@@ -26,8 +22,8 @@ var postOrder = {
     var target = node.callee;
     if (target.type === 'MemberExpression' &&
         target.property.type === 'Identifier' &&
-        _.has(methods, target.property.name)) {
-      this.found[target.property.name] = true;
+        _.contains(methods, target.property.name)) {
+      this.found.push(target.property.name);
     }
   },
   MemberExpression: function(node) {
@@ -35,7 +31,7 @@ var postOrder = {
     var key = node.property;
     if (target.type === 'Identifier' && target.name === '_' &&
         key.type === 'Identifier' && isImplicit(target, this.scopes)) {
-      this.found[key.name] = true;
+      this.found.push(key.name);
     }
   }
 };
@@ -44,7 +40,7 @@ var postOrder = {
 module.exports = function(ast) {
   var context = {
     scopes: escope.analyze(ast).scopes,
-    found: {}
+    found: []
   };
   traverse(ast, {
     post: function(node) {
@@ -53,5 +49,5 @@ module.exports = function(ast) {
       }
     }
   });
-  return Object.keys(context.found).sort();
+  return _.uniq(context.found).sort();
 };
